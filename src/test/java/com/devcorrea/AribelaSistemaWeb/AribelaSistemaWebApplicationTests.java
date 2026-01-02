6216f83de6e4b7b140395686c4e0fd786601ff50
correa <jovicorreabz@gmail.com>
2026-01-01 21:26:50 -0400

    feat - add mvn, scr/main etc..

---
+++ src/test/java/com/devcorrea/AribelaSistemaWeb/AribelaSistemaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcorrea.AribelaSistemaWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AribelaSistemaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
